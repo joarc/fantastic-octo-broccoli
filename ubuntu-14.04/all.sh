1546153f8bfeb5cf209b4b31a8347689a0d6e74a
Joar Classon <joarc@joarc.se>
2016-02-24 13:07:51 +0100

    Cleaned output of devloop and also added load reading

--- ubuntu-14.04/all.sh
+++ ubuntu-14.04/all.sh
@@ -4,7 +4,18 @@
 ##  Use on own risk
 ##
 
+## Start message
+echo "¤--------------------¤"
+echo "|                    |"
+echo "|   General Report   |"
+echo "|                    |"
+echo "¤--------------------¤"
+date
+echo "----------------------"
+echo ""; echo ""
+
 ## Get all packages that need upgrading
+echo "-[ Packages ]-"
 echo "These packages need to be updated:"
 echo ""
 apt-get -qq -s upgrade > apt-upgrade.log
@@ -14,3 +25,29 @@ while read p; do
     echo $p |cut -d ' ' -f2,3
   fi
 done <apt-upgrade.log
+echo ""; echo ""
+
+## Get server load
+echo "-[ Server Load ]-"
+echo ""
+cpus=$(nproc)
+loadavg=$(cat /proc/loadavg |cut -d ' ' -f1,2,3)
+loadf=$(echo $loadavg | cut -d ' ' -f1)
+loads=$(echo $loadavg | cut -d ' ' -f2)
+loadt=$(echo $loadavg | cut -d ' ' -f3)
+if (( $(echo "$loadf >= $cpus" | bc -l) )); then
+  echo "In the previous minute, load has been over the amount of cpus that this server has!"
+elif (( $(echo "$loads >= $cpus" | bc -l) )); then
+  echo "In the previous 5 minutes, load has been over the amount of cpus that this server has!"
+elif (( $(echo "$loadt >= $cpus" | bc -l) )); then
+  echo "In the previous 15 minutes, load has been over the amount of cpus that this server has!"
+else
+  echo "Load has been under the amount of cpus that this server has."
+fi
+echo "Load 1m: $loadf, Load 5m: $loads, Load 15m: $loadt"
+echo "CPU Amount: $cpus"
+echo ""
+## Harddrive
+harddrives=$(df -h)
+echo 
+echo ""; echo ""
